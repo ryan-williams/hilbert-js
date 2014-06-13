3f10e2a0f213a77c696453392e91184b3780a2ad
Ryan Williams <ryan.blake.williams@gmail.com>
2014-06-12 20:26:07 -0400

    add module-level exports

---
+++ hilbert.js
@@ -0,0 +1,8 @@
+
+exports.d2xy = require('./hilbert2d').d2xy;
+exports.xy2d = require('./hilbert2d').xy2d;
+
+exports.d2xyz = require('./hilbert3d').d2xyz;
+exports.xyz2d = require('./hilbert3d').xyz2d;
+
+exports.Point = require('./point').Point;
